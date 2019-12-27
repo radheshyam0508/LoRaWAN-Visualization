b3d0ac7ce285cc967525947aed2332cd9c42737d
radheshyam0508 <38689467+radheshyam0508@users.noreply.github.com>
2019-12-27 16:00:45 +0100

    Add files via upload

---
+++ Python Code for SODAQ SARA-n211/RunPSM.py
@@ -0,0 +1,4 @@
+import time,os
+while 1:
+    time.sleep(10)
+    os.system("start py -2 saraPSM.py -kanmct") 
---
+++ Python Code for SODAQ SARA-n211/run_30.py
@@ -0,0 +1,4 @@
+import time,os
+while 1:
+    time.sleep(60)
+    os.system("start py -2 sara2.py -kancvt") 
